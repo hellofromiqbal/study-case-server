26c9d655df1050ad6e657a8463f97b78390f78ac
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-21 11:20:19 +0700

    add productController.update

--- app/product/controller.js
+++ app/product/controller.js
@@ -73,7 +73,73 @@ const index = async (req, res, next) => {
   };
 };
 
+const update = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let payload = req.body;
+
+    if(req.file) {
+      let tmp_path = req.file.path;
+      let originalExt = req.file.originalname.split('.').pop();
+      let filename = req.file.filename + '.' + originalExt;
+      let target_path = path.resolve(config.rootPath, `public/images/products/${filename}`);
+
+      const src = fs.createReadStream(tmp_path);
+      const dest = fs.createWriteStream(target_path);
+      src.pipe(dest);
+
+      src.on('end', async () => {
+        try {
+          let newProduct = await Product.findByIdAndUpdate(
+            id,
+            { ...payload, image_url: filename },
+            { new: true, runValidators: true }
+          );
+          return res.json({
+            message: 'New product added!',
+            data: newProduct
+          });
+        } catch (err) {
+          fs.unlinkSync(target_path);
+          if(err && err.name === 'ValidationError') {
+            return res.json({
+              error: 1,
+              message: err.message,
+              fields: err.errors
+            });
+          };
+          next(err);
+        };
+      });
+
+      src.on('error', async (err) => {
+        next(err);
+      });
+    } else {
+      let newProduct = await Product.findByIdAndUpdate(
+        id,
+        payload,
+        { new: true, runValidators: true }
+      );
+      return res.json({
+        message: 'New product added!',
+        data: newProduct
+      });
+    };
+  } catch (err) {
+    if(err && err.name === 'ValidationError') {
+      return res.json({
+        error: 1,
+        message: err.message,
+        fields: err.errors
+      });
+    }
+    next(err);
+  };
+};
+
 module.exports = {
   store,
-  index
+  index,
+  update
 };
