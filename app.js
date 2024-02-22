44efcf7bea0a97158b8fa1f1e55519e54d92dc24
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-22 22:13:36 +0700

    add login, logout (not working)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const { decodeToken } = require('./middlewares');
 const productRouter = require('./app/product/router');
 const categoryRouter = require('./app/category/router');
 const tagRouter = require('./app/tag/router');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(decodeToken());
 
 app.use('/auth', authRouter);
 app.use('/api', productRouter);
--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,4 +1,9 @@
 const User = require('../user/model');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const { getToken } = require('../../utils');
 
 const register = async (req, res, next) =>  {
   try {
@@ -21,6 +26,63 @@ const register = async (req, res, next) =>  {
   };
 };
 
+const localStrategy = async (email, password, done) => {
+  try {
+    const user = await User
+      .findOne({ email })
+      .select('-__v -createdAt -updatedAt -cart_items -token');
+    if(!user) return done();
+    if(bcrypt.compareSync(password, user.password)) {
+      ( { password, ...userWithoutPassword } = user.toJSON() );
+      return done(null, userWithoutPassword);
+    }
+  } catch (error) {
+    done(err, null);
+  };
+  done();
+};
+
+const login = async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if(err) return next(err);
+    if(!user) return res.json({
+      error: 1,
+      message: 'Invalid email/password.'
+    });
+    
+    const signed = jwt.sign(user, config.secretKey);
+
+    await User.findByIdAndUpdate(user._id, { $push: { token: signed } });
+
+    return res.json({
+      message: 'Login berhasil!',
+      data: user,
+      token: signed
+    });
+  })(req, res, next);
+};
+
+const logout = async (req, res, next) => {
+  let token = getToken(req);
+
+  let user = await User.findOneAndUpdate({ token: { $in: { token: [token] } } }, { $pull: { token: token } }, { useFindAndModify: false });
+
+  if(!token || !user) {
+    res.json({
+      error: 1,
+      message: 'User not found!'
+    });
+  };
+
+  return res.json({
+    error: 0,
+    message: 'Logout berhasil!'
+  });
+};
+
 module.exports = {
-  register
+  register,
+  login,
+  localStrategy,
+  logout
 };
